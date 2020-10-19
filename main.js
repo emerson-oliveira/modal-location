6fb9be825bd5f8c1b51a4c291219d6abdde4167f
Mateus Silva <mateusferreira17@yahoo.com.br>
2020-10-18 21:31:39 -0300

    mask e a regiao

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+const regiao_1 = ['MG']
+const regiao_2 = ['SP']
+const regiao_3 = ['RJ']
 
 $(document).ready(function() {
+    $('input[name=\"zipcode\"]').mask("99999-999");
 
-    $("#teste").attr("disabled", true);
-    $("#teste").hide();
+    $("#teste").attr("disabled", true).hide();
     $('input[name="zipcode"]').on('keyup', function() {
-        if(this.value.length===8){
+        let cepCodigo = this.value
+        if(cepCodigo.length===9){
             const request = new Request('https://viacep.com.br/ws/'+this.value+'/json/');
 
             fetch(request)
@@ -16,11 +20,36 @@ $(document).ready(function() {
                     }
                 })
                 .then(data => {
-                    $("#teste").attr("disabled", false);
-                    $("#teste").show();
-                    $('#stateForm').val(data['uf']);
-                    console.log(data['uf']);
+                    if(data['erro'])
+                        alert("Formato de CEP inválido.");
+                    else{
+                        $("#teste").attr("disabled", false).show();
+
+                        $('#stateForm').val(data['uf']);
+
+
+                        console.log(cepCodigo[0],'posicao');
+                        checa_regiao(data['uf']);
+                    }
+
                 });
         }
     });
+    $("#stateForm").change(function () {
+        checa_regiao(this.value)
+
+    });
 });
+
+
+function checa_regiao(estado) {
+
+    if(regiao_1.includes(estado)){
+        console.log('regiao 1')
+    }else if(regiao_2.includes(estado)){
+        console.log('regiao 2')
+    }else {
+        console.log('regiao 3')
+    }
+
+}
